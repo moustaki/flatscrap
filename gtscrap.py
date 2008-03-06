7287e66d88e9d5392567a2ebaa196c3920c3b89c
Yves Raimond <yves@dbtune.org>
2008-03-06 18:34:12 +0000

    email

--- gtscrap.py
+++ gtscrap.py
@@ -11,14 +11,28 @@ f.close()
 
 soup = BeautifulSoup(html)
 
+def clean(atom):
+        t1 = ''.join(atom.rsplit('&nbsp;'))
+        t2 = ''.join(t1.rsplit('\n'))
+        return t2
+
+
 # Now, let's scrap!
 
-location =  soup('span','location')[0].contents[1][6:]
-title = soup('div',id="title")[0].contents[0].contents[0]
+location = clean(soup('span','location')[0].contents[1])
+title = clean(soup('div',id="title")[0].contents[0].contents[0])
 description = soup('div',id="desc")[0].contents[0].contents[0].contents[0]
+email1 = soup('span','email')[0].contents[2].attrs[0][1]
+if email1.startswith('/cgi-bin'):
+	email = "http://www.gumtree.com"+email1
+else :
+	email = email1
+#tel = clean(soup('div',id="replyto")[0].contents[0].contents[3])
+
 
 print location
 print title
 print description
+print email
 
 
