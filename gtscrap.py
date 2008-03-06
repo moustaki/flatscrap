18801149edcbb9815f5c155694c0cb342efb9642
Yves Raimond <yves@dbtune.org>
2008-03-06 18:09:24 +0000

    Now scraps title and description

--- gtscrap.py
+++ gtscrap.py
@@ -11,10 +11,14 @@ f.close()
 
 soup = BeautifulSoup(html)
 
+# Now, let's scrap!
+
 location =  soup('span','location')[0].contents[1][6:]
+title = soup('div',id="title")[0].contents[0].contents[0]
+description = soup('div',id="desc")[0].contents[0].contents[0].contents[0]
 
 print location
-
-
+print title
+print description
 
 
